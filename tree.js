8229ec0c343b85e6c1545eac064d7465269e6549
Justin Elliott <thecrashtestjustin@gmail.com>
2023-04-29 22:04:06 -0700

    updated binary search tree algorithm to use class

---
+++ tree.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    const sorted = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.buildTree(sorted, 0, sorted.length - 1);
+  }
+
+  buildTree(array, start, end) {
+    if (start > end) {
+      return null;
+    } else {
+      let mid = parseInt((start + end) / 2);
+      let node = new Node(array[mid]);
+      node.left = this.buildTree(array, start, mid - 1);
+      node.right = this.buildTree(array, mid + 1, end);
+      return node;
+    }
+  }
+
+  insert(data, root = this.root) {
+    if (root === null) {
+      root = new Node(data);
+      return root;
+    }
+    if (root.data > data) {
+      root.left = this.insert(data, root.left);
+    } else if (root.data < data) {
+      root.right = this.insert(data, root.right);
+    }
+    return root;
+  }
+}
+
+//TEST RUN
+
+let testArray = [2, 9, 8, 5, 4, 7, 4, 3, 6, 1];
+let testTree = new Tree(testArray);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null && node.right !== undefined) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null && node.left !== undefined) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(testTree.root);
+console.log(testTree.root);
+console.log(testTree.insert(99));
+console.log('inserted"');
+prettyPrint(testTree.root);
+console.log(testTree.root);
